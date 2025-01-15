4d63b43dbec1651c13a3197bd884f3d4f5fa9d0c
Pp0y <96282846+Pp0y@users.noreply.github.com>
2025-01-15 16:18:48 +0700

    Fix port

--- server.py
+++ server.py
@@ -25,4 +25,5 @@ def upload_image():
     return jsonify({"message": "File saved successfully!", "file_path": file_path}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
