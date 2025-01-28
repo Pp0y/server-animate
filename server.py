8fb4a983560c7da11a340715f6d90a430559030c
Pp0y <96282846+Pp0y@users.noreply.github.com>
2025-01-28 17:35:02 +0700

    Change port from 5000 to 8000

--- server.py
+++ server.py
@@ -84,5 +84,5 @@ def process():
     return Response(response.getvalue(), mimetype='image/jpeg')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host="0.0.0.0", port=port)
